67fd800cd8a6ef6b7a5612b9fb3999c7f76607fc
Frederick <freddychan@live.com>
2019-05-07 15:21:07 -0700

    added cmd to play audio with yt links

--- commands/playYTAudio.js
+++ commands/playYTAudio.js
@@ -1,50 +1,49 @@
 const Discord = module.require("discord.js");
 const ytdl = require("ytdl-core");
 
-module.exports.run = async (bot, message, args) => {
-	console.log("in play cmd");
-	const playargs = message.content.split(/\s+/g);
+// Calls bot to join voice channel and begin playing audio from youtube link
+module.exports.run = async (message) => {
+	//console.log("in play cmd");
+	const args = message.content.split(/\s+/g);
 	const voiceChannel = message.member.voiceChannel;
-	const queue = message.client.queue;
-	const serverQueue = message.client.queue.get(message.guild.id);
 		
 	if (!voiceChannel) return message.reply("I must be in a voice channel.");
 		
-	const audioInfo = await ytdl.getInfo(playargs[1]);
-	const song = {
+	const audioInfo = await ytdl.getInfo(args[2]);
+	const audio = {
 		title: audioInfo.title,
 		url: audioInfo.video_url,
 	};
 		
-	if (!serverQueue) {
-		const queueContruct = {
-			textChannel: message.channel,
-			voiceChannel: voiceChannel,
-			connection: null,
-			songs: [],
-			volume: 5,
-			playing: true,
-		};
+	const queue = {
+		textChannel: message.channel,
+		voiceChannel: voiceChannel,
+		connection: null,
+		songs: [],
+		volume: 5,
+		playing: true,
+	};
 
-		queue.set(message.guild.id, queueContruct);
+	queue.songs.push(audio);
 
-		queueContruct.songs.push(song);
-
-		try {
-			var connection = await voiceChannel.join();
-			queueContruct.connection = connection;
-			this.play(message, queueContruct.songs[0]);
+	try {
+		var connection = await voiceChannel.join();
+		queue.connection = connection;
+		const dispatcher = connection.playStream(ytdl(audio.url))
+			.on('end', () => {
+				console.log('Music ended!');
+			})
+				.on('error', error => {
+					console.error(error);
+			});
+		message.channel.send("Playing " + audio.title);
 		} catch (err) {
 			console.log(err);
-			queue.delete(message.guild.id);
 			return message.channel.send(err);
 		}
-	} else {
-		serverQueue.songs.push(song);
-		return message.channel.send(`${song.title} has been added to the queue!`);
-	}
 }
 
+// Call command with !db play "youtube-url"
 module.exports.help = {
 	name: "play"
 }
